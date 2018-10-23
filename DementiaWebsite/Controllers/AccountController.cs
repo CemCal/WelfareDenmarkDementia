45296fed7355e628917c32e11a227f92d3519199
Cem Calgici <Cemox35@live.dk>
2018-10-23 17:50:26 +0200

    q

---
+++ DementiaWebsite/Controllers/AccountController.cs
@@ -0,0 +1,59 @@
+﻿using DementiaWebsite.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace DementiaWebsite.Controllers
+{
+
+    
+    public class AccountController : Controller
+    {
+
+        private SignInManager<RegisterViewModel> _signManager;
+        private UserManager<RegisterViewModel> _userManager;
+
+        public AccountController(UserManager<RegisterViewModel> userManager, SignInManager<RegisterViewModel> signManager)
+        {
+            _userManager = userManager;
+            _signManager = signManager;
+        }
+
+        [HttpGet]
+        public ViewResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new RegisterViewModel { UserName = model.UserName };
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await _signManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View();
+        }
+
+
+    }
+}
